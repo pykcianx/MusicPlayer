ecb69db5fdad55d64554feff48185050b158d8e2
pykcianx <sznapi221@wp.pl>
2021-11-12 15:13:35 +0100

    commit1

--- mp3app.py
+++ mp3app.py
@@ -8,33 +8,30 @@ from mutagen.mp3 import MP3
 import tkinter.ttk as ttk
 
 root = Tk()
-root.title('mp3app')
+root.title('AudioPlayer')
 root.iconbitmap('mp3player\\mp3.ico')
-root.geometry('500x450')
+root.geometry('470x600')
+root.resizable(width=0, height=0)
 
 pygame.mixer.init()
 
 
-#ADD_SONG_FUNC
 def add_song():
-    song = filedialog.askopenfilename(title="Wybierz plik", filetypes=(("mp3 Files", "*.mp3"),))
-
-    #dict = {"song": "os.path.basename(song)"}
-
-    #song_name = os.path.basename(song)
+    song = filedialog.askopenfilename(title="Wybierz plik", filetypes=(("mp3 Files", "*.mp3"),("wav Files", "*.wav")))
     song_list.insert(END, song)
 
-    #python add file to dict or add path to dict and getfilename
-
-#ADD_MANY_SONGS
 def add_songs():
     songs = filedialog.askopenfilenames(title="Wybierz pliki", filetypes=(("mp3 Files", "*.mp3"),))
 
     for song in songs:
         song_list.insert(END, song)
 
-#PLAY_SONG
 def play():
+    global stopped
+    stopped = False
+
+    music_slider.config(value=0)
+
     song_to_play = song_list.get(ACTIVE)
 
     pygame.mixer.music.load(song_to_play)
@@ -42,20 +39,28 @@ def play():
 
     play_time()
 
-    #update slider
-    #slider_position = int(song_length)
-    #music_slider.config(to=slider_position, value=0)
+    current_volume = pygame.mixer.music.get_volume()
+    volume_label.config(text=current_volume * 100)
 
-#STOP_SONG
+
+
+global stopped
+stopped = False
 def stop():
+    song_status.config(text='')
+    music_slider.config(value=0)
+
     pygame.mixer.music.stop()
     song_list.selection_clear(ACTIVE)
     song_status.config(text='')
 
+    global stopped
+    stopped = True
+
+
+
 global paused
 paused = False
-
-#PAUSE_SONG
 def pause(is_paused):
     global paused
     paused = is_paused
@@ -67,8 +72,10 @@ def pause(is_paused):
         pygame.mixer.music.pause()
         paused = True
 
-#PLAY_NEXT_SONG
 def next_song():
+    song_status.config(text='')
+    music_slider.config(value=0)
+
     current_song = song_list.curselection()
     next_one = current_song[0]+1
     song = song_list.get(next_one)
@@ -76,13 +83,14 @@ def next_song():
     pygame.mixer.music.load(song)
     pygame.mixer.music.play(loops=0)
     
-    #MOVE_ACTIVE_BAR
     song_list.selection_clear(0, END)
     song_list.activate(next_one)
     song_list.selection_set(next_one, last=None)
 
-#PLAY_PREVIOUS_SONG
 def previous_song():
+    song_status.config(text='')
+    music_slider.config(value=0)
+
     current_song = song_list.curselection()
     previous_one = current_song[0]-1
     song = song_list.get(previous_one)
@@ -90,69 +98,92 @@ def previous_song():
     pygame.mixer.music.load(song)
     pygame.mixer.music.play(loops=0)
     
-    #MOVE_ACTIVE_BAR
     song_list.selection_clear(0, END)
     song_list.activate(previous_one)
     song_list.selection_set(previous_one, last=None)
 
-#SONG_TIME
 def play_time():
+    if stopped:
+        return
+
     current_time = pygame.mixer.music.get_pos() / 1000
     clock_time = time.strftime('%H:%M:%S', time.gmtime(current_time))
 
-    slider_label.config(text=f'Slider: {int(music_slider.get())} and Song Pos: {int(current_time)}')
-
-    current_song = song_list.curselection()
-    song = song_list.get(current_song)
+    song = song_list.get(ACTIVE)
     load_song = MP3(song)
 
     global song_length
     song_length = load_song.info.length
     clock_time_length = time.strftime('%H:%M:%S', time.gmtime(song_length))
 
-    song_status.config(text=f'Upłynęło: {clock_time}  z  {clock_time_length}  ')
-    #update slider position to song position
-    music_slider.config(value=current_time)
-
-    slider_position = int(song_length)
     current_time +=1
-    music_slider.config(to=slider_position, value=int(current_time))
+    if int(music_slider.get()) == int(song_length):
+        song_status.config(text=f'Upłynęło: {clock_time_length}')
+        next_song()
 
-    #updating time every 1000ms
+    elif paused:
+        pass
+
+    elif int(music_slider.get()) == int(current_time):
+        slider_position = int(song_length)
+        music_slider.config(to=slider_position, value=int(current_time))
+
+    else:
+        slider_position = int(song_length)
+        music_slider.config(to=slider_position, value=int(music_slider.get()))
+        clock_time = time.strftime('%H:%M:%S', time.gmtime(int(music_slider.get())))
+        song_status.config(text=f'Upłynęło: {clock_time}  z  {clock_time_length}  ')
+        next_time = int(music_slider.get()) + 1
+        music_slider.config(value=next_time)
+
+
     song_status.after(1000, play_time)
 
-#DELETE_SONG
 def del_song():
+    stop()
     song_list.delete(ANCHOR)
     pygame.mixer.music.stop()
 
-#DELETE_SONGS
 def del_songs():
+    stop()
     song_list.delete(0, END)
     pygame.mixer.music.stop()
 
 def slide(x):
-    #slider_label.config(text=f'{int(music_slider.get())} of {int(song_length)}')
     song_to_play = song_list.get(ACTIVE)
 
     pygame.mixer.music.load(song_to_play)
     pygame.mixer.music.play(loops=0, start=int(music_slider.get()))
 
+def volume(x):
+    pygame.mixer.music.set_volume(volume_slider.get())
+    current_volume = pygame.mixer.music.get_volume()
+    volume_label.config(text=current_volume * 100)
+
+
+
+master_frame = Frame(root)
+master_frame.pack(pady=20)
 
 #SONG_LIST
-song_list = Listbox(root, bg="black", fg="green", width=60, selectbackground="gray", selectforeground="black")
-song_list.pack(pady=20)
+song_list = Listbox(master_frame, bg="black", fg="green", width=60, height=25, selectbackground="#e56353", selectforeground="white", background="#313131", foreground="white")
+song_list.grid(row=0, column=0)
 
 #CONTROL BTNS
 #BTN_IMAGES
-back_btn_img = PhotoImage(file="mp3player\\backward_btn.png")
-forward_btn_img = PhotoImage(file="mp3player\\forward_btn.png")
-play_btn_img = PhotoImage(file="mp3player\\play_btn.png")
-pause_btn_img = PhotoImage(file="mp3player\\pause_btn.png")
-stop_btn_img = PhotoImage(file="mp3player\\stop_btn.png")
+back_btn_img = PhotoImage(file="mp3player\\back.png")
+forward_btn_img = PhotoImage(file="mp3player\\forward.png")
+play_btn_img = PhotoImage(file="mp3player\\play.png")
+pause_btn_img = PhotoImage(file="mp3player\\pause.png")
+stop_btn_img = PhotoImage(file="mp3player\\stop.png")
+
 #BTN_FRAME
-controls_frame = Frame(root)
-controls_frame.pack()
+controls_frame = Frame(master_frame)
+controls_frame.grid(row=1, column=0)
+
+volume_frame = LabelFrame(master_frame, text="Głośność")
+volume_frame.grid(row=0, column=1, padx=10)
+
 #BUTTONS
 back_btn = Button(controls_frame, image=back_btn_img, borderwidth=0, command = previous_song)
 forward_btn = Button(controls_frame, image=forward_btn_img, borderwidth=0, command = next_song)
@@ -168,28 +199,32 @@ stop_btn.grid(row=0, column=4, padx=5)
 
 #STATUS_BAR
 song_status = Label(root, text='', bd=1, relief=GROOVE, anchor=E)
-song_status.pack(fill=X, side=BOTTOM, ipady=2)
+song_status.pack(fill=X, side=BOTTOM, ipady=0)
 
-#SLIDER
-music_slider = ttk.Scale(root, from_=0, to=100, orient=HORIZONTAL, value=0, command=slide, length=360)
-music_slider.pack(pady=40)
+#SLIDERS
+music_slider = ttk.Scale(master_frame, from_=0, to=100, orient=HORIZONTAL, value=0, command=slide, length=360)
+music_slider.grid(row=3, column=0, pady=20)
 
-slider_label = Label(root, text="0")
-slider_label.pack(pady=10)
+volume_slider = ttk.Scale(volume_frame, from_=1, to=0, orient=VERTICAL, value=1, command=volume, length=125)
+volume_slider.pack(pady=10)
 
+volume_label = Label(volume_frame, text="0")
+volume_label.pack(pady=10)
 
 #MENU
 my_menu = Menu(root)
 root.config(menu=my_menu)
 
 #ADD_SONGS_MENU
-add_song_menu = Menu(my_menu)
+add_song_menu = Menu(my_menu, tearoff = False)
 my_menu.add_cascade(label="Plik", menu=add_song_menu)
 add_song_menu.add_command(label="Dodaj plik audio", command=add_song)
 #ADD_MANY_SONGS
 add_song_menu.add_command(label="Dodaj wiele plików audio", command=add_songs)
+add_song_menu.add_command(label = "_________________________________")
+add_song_menu.add_command(label = "Zamknij program", command = root.destroy)
 #DELETE_SONG_MENU
-remove_song_menu = Menu(my_menu)
+remove_song_menu = Menu(my_menu, tearoff = False)
 my_menu.add_cascade(label="Edycja", menu=remove_song_menu)
 remove_song_menu.add_command(label="Usuń piosenkę", command=del_song)
 remove_song_menu.add_command(label="Usuń wszystkie piosenki", command=del_songs)
